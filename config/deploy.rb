3cdb541c4bf030fd8220fa2091b954181ee345b3
takei-teppei <boxmaikumanny348@gmail.com>
2020-01-25 17:37:16 +0900

    setting_deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,4 +31,19 @@ namespace :deploy do
   task :restart do
     invoke 'unicorn:restart'
   end
+
+# master.key用のシンボリックリンクを追加
+set :linked_files, %w{ config/master.key }
+
+  desc 'upload master.key'
+  task :upload do
+    on roles(:app) do |host|
+      if test "[ ! -d #{shared_path}/config ]"
+        execute "mkdir -p #{shared_path}/config"
+      end
+      upload!('config/master.key', "#{shared_path}/config/master.key")
+    end
+  end
+  before :starting, 'deploy:upload'
+  after :finishing, 'deploy:cleanup'
 end

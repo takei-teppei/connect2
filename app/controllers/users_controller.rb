93057fffb853994f2d6c85f717d7c944f3d8ee48
takei-teppei <boxmaikumanny348@gmail.com>
2020-02-03 00:56:24 +0900

    setting_search

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,14 @@ class UsersController < ApplicationController
     @resumes = current_user.resumes.includes(:user)
   end
 
+  def search
+    return nil if params[:keyword] == ""
+    @users = User.where(['name LIKE ?', "%#{params[:keyword]}%"] ).where.not(id: current_user.id).limit(10)
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
   private
 
   def move_to_index

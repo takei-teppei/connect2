2ba0444c277a28b43b3bbc9ee4142aee8ee166e0
takei-teppei <boxmaikumanny348@gmail.com>
2020-02-12 15:55:50 +0900

    setting_index_js

--- app/assets/javascripts/index_scroll.js
+++ app/assets/javascripts/index_scroll.js
@@ -54,4 +54,18 @@ $(function(){
     });
   });
 });
+$(function(){
+  $(window).scroll(function (){
+    $("#sample5").each(function(){
+      var imgPos = $(this).offset().top;
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll > imgPos - windowHeight + windowHeight/5){
+        $(this).addClass("fade_on");
+      } else {
+        $(this).removeClass("fade_on");
+      }
+    });
+  });
+});
   
